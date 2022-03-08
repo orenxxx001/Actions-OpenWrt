f66f0b42e791b46f1ba402b7c126897db1009567
orenxxx001 <33869335+orenxxx001@users.noreply.github.com>
2022-03-08 21:45:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -5,6 +5,9 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #============================================================
 
+# 更改默认密码
+sed -i 's/root::0:0:99999:7:::/root:$1$MhPcOOTE$DOOyDUwKjP9xnoSfaczsk.:19058:0:99999:7:::7g' package/base-files/files/etc/shadow
+
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.88.8/g' package/base-files/files/bin/config_generate
 
@@ -14,11 +17,8 @@ sed -i 's/OpenWrt/XinV-2.0/g' package/base-files/files/bin/config_generate
 # Modify the version number版本号里显示一个自己的名字（AutoBuild $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
 sed -i 's/OpenWrt /Build $(TZ=UTC-8 date "+%Y.%m.%d") @ XinV-2.0 /g' package/lean/default-settings/files/zzz-default-settings
 
-# 修改主机名字，把Xiaomi-R4A修改你喜欢的就行（不能纯数字或者使用中文）
+# 修改主机名字，把XinV-2.0修改你喜欢的就行（不能纯数字或者使用中文）
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='XinV-2.0'' package/lean/default-settings/files/zzz-default-settings
-
-# 拉取 darkmatter/luci-theme-jj 原作者的源码，放到 package 目录下，编译时会自动遍历
-git clone https://https://github.com/jerryc127/hexo-theme-butterfly package/lean/butterfly
 
 # 删除自定义源默认的 argon、bootstrap 主题
 rm -rf package/lean/luci-theme-argon
