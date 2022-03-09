a250152464af1339d507a2289caa214795cf82a8
orenxxx001 <33869335+orenxxx001@users.noreply.github.com>
2022-03-09 16:17:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -5,19 +5,6 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #============================================================
 
-wifi_name='RR'
-wifi_encryption='psk2'
-wifi_password='23456789DDop'
-
-echo "修改wifi名称"
-sed -i "s/OpenWrt/$wifi_name/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
-
-echo "修改wifi加密方式"
-sed -i "s/none/$wifi_encryption/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
-
-echo "修改wifi加密密码"
-sed -i '/set wireless.default_radio${devidx}.encryption=none/aset wireless.default_radio${devidx}.key=$wifi_password' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-
 # 更改默认密码
 sed -i 's/root::0:0:99999:7:::/root:$1$MhPcOOTE$DOOyDUwKjP9xnoSfaczsk.:19058:0:99999:7:::/g' package/base-files/files/etc/shadow
 
