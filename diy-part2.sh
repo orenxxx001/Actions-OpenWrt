c7c3f12cae5644ced41dc53f52979c6981fa57c0
orenxxx001 <33869335+orenxxx001@users.noreply.github.com>
2022-03-09 20:31:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,10 +27,6 @@ rm -rf package/lean/luci-theme-bootstrap
 # 添加主题
 git clone darkmatter git://github.com/apollo-ng/luci-theme-darkmatter.git package/lean/luci-theme-darkmatter
 
-# 取消bootstrap为默认主题：
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/neobird/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-neobird
-
 # 替换默认主题为 luci-theme-darkmatter
 # sed -i 's/更改前的信息/更改后的信息/g' ./要修改的文件的目录（可以用本地查看）
 sed -i 's/luci-theme-bootstrap/luci-theme-darkmatter/g' feeds/luci/collections/luci/Makefile
