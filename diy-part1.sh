e689e347597a6e2bbe0da04f36934da16d933af4
orenxxx001 <33869335+orenxxx001@users.noreply.github.com>
2022-03-11 00:51:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,6 +21,3 @@ sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # Add luci-theme-darkmatter
 sed -i '$a src-git darkmatter https://github.com/apollo-ng/luci-theme-darkmatter.git' feeds.conf.default
-
-# 替换默认主题为 luci-theme-darkmatter
-sed -i 's/luci-theme-bootstrap/luci-theme-darkmatter/g' feeds/luci/collections/luci/Makefile
